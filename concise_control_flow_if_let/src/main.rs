39bb50f580d814f806ca8fa567c4f00ba29e8875
nenjotsu <renenjo.valente@gmail.com>
2022-01-29 19:43:25 +0800

    add if let

---
+++ concise_control_flow_if_let/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    refactor_using_iflet();
+
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("max is config to be {max}"),
+        _ => (),
+    }
+}
+
+fn refactor_using_iflet() {
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("max is config to be {max}");
+    }
+}
+
+fn using_match() {
+    let mut count = 0;
+    match coin {
+        Coin::Quarter(state) => println!("state quarter from {:?}!", state),
+        _ => count += 1,
+    }
+}
+
+fn using_if_let_else() {
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("state quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+}
---
+++ doc_rust_lang.md
@@ -0,0 +1,5 @@
+
+# 6. Enums and Pattern Matching
+## 6.1 Defining an Enum
+## 6.2 The march Control Flow Operator
+## 6.3 Concise Control Flow with if let
